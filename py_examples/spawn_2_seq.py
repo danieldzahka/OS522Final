af9c7b2845a0984234de3e48dbe0b33c127875f2
danieldzahka <daniel.zahka@wustl.edu>
2018-04-22 12:22:56 -0500

    trying to fix git problems

--- py_examples/spawn_2_seq.py
+++ py_examples/spawn_2_seq.py
@@ -5,18 +5,13 @@ Daniel Zahka ~ daniel.zahka@wustl.edu
 Anish Naik ~ anish.r.naik@wustl.edu
 """
 
-import time
-
 def thread_func():
         for i in xrange(1, 50000000):
                 k = i * 3
 
 
 if __name__ == "__main__":
-    print("Spawning Threads...")
-    start = time.time()
+    print("Starting work...")
     thread_func()
     thread_func()
-    end = time.time()
-    print("Joined Threads...")
-    print(end - start)
+    print("Done...")
